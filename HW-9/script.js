23aa984c45fdebd0568c2a899d29b377d6a8c49a
Oleg Grynkevych <oleg252.og@gmail>
2022-06-18 15:34:57 +0300

    Added homework-9

---
+++ HW-9/script.js
@@ -0,0 +1,19 @@
+function getRandomInt() {
+    return Math.floor(Math.random() * 256);
+  }
+
+function generateBlocksInterval () {
+    const block = document.createElement("div");
+    block.className = "box";
+    document.body.append(block);
+
+    for (let el = 0; el < 25; el++) {
+        const smallBlock = document.createElement("div");
+        smallBlock.className = "smallbox";
+        block.append(smallBlock);
+
+        setInterval(() => smallBlock.style.background = `rgb(${getRandomInt()}, ${getRandomInt()}, ${getRandomInt()})`, 1000);
+    }
+}
+
+generateBlocksInterval();
